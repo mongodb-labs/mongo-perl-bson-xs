377f4f64fdaea1868166204a58224501b50c1923
David Golden <xdg@xdg.me>
2017-12-05 18:22:07 -0500

    PERL-744 PERL-804 PERL-830 CI: Update ZAP testing config
    This commit does the following:
    
    * Adds ZAP testing
    * Harmonizes test variants with Perl driver and BSON.pm
    
    The latter includes:
    * No "stepback" on ZAP
    * Long batch time on ZAP
    * No long-double testing on Z-series

--- .evergreen/testing/write-config.pl
+++ .evergreen/testing/write-config.pl
@@ -10,21 +10,36 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib";
 use EvergreenConfig;
 
+# Limit tasks to certain operating systems
+my $OS_FILTER =
+  { os =>
+      [ 'rhel62', 'windows64', 'suse12_z', 'ubuntu1604_arm64', 'ubuntu1604_power8' ] };
+
 sub main {
+    my $download = [ 'downloadPerl5Lib' => { target => '${repo_directory}' } ];
+
     my @tasks = (
-        pre(qw/dynamicVars cleanUp fetchSource downloadPerl5Lib/),
+        pre( qw/dynamicVars cleanUp fetchSource/, $download ),
         post(qw/cleanUp/),
-        task( build => [qw/whichPerl buildModule uploadBuildArtifacts/] ),
+        task(
+            build  => [qw/whichPerl buildModule uploadBuildArtifacts/],
+            filter => $OS_FILTER
+        ),
         task(
             test       => [qw/whichPerl downloadBuildArtifacts testModule/],
-            depends_on => 'build'
+            depends_on => 'build',
+            filter     => $OS_FILTER,
         ),
     );
 
-    print assemble_yaml(
-        timeout(600),
-        buildvariants( \@tasks ),
-    );
+    # Build filter to avoid "ld" Perls on Z-series
+    my $variant_filter = sub {
+        my ( $os, $ver ) = @_;
+        return 0 if $os eq 'suse12_z' && $ver =~ m/ld$/;
+        return 1;
+    };
+
+    print assemble_yaml( timeout(600), buildvariants( \@tasks, $variant_filter ), );
 
     return 0;
 }
