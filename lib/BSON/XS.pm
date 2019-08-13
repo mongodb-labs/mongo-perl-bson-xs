61d489aad5d71db521e888b89e7c18a7d8a53985
David Golden <xdg@xdg.me>
2019-08-13 08:21:32 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/BSON/XS.pm
+++ lib/BSON/XS.pm
@@ -21,7 +21,7 @@ package BSON::XS;
 # ABSTRACT: XS implementation of MongoDB's BSON serialization
 
 use version;
-our $VERSION = 'v0.8.1';
+our $VERSION = 'v0.8.2';
 
 # cached for efficiency during decoding
 # XXX eventually move this into XS
