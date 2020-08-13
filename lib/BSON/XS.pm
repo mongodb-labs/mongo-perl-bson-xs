8a7db1d4d97d424197b1b7308c5face433e9af00
David Golden <xdg@xdg.me>
2020-08-13 10:50:33 -0400

    Prepare EOL release

--- lib/BSON/XS.pm
+++ lib/BSON/XS.pm
@@ -18,7 +18,7 @@ use strict;
 use warnings;
 
 package BSON::XS;
-# ABSTRACT: XS implementation of MongoDB's BSON serialization
+# ABSTRACT: XS implementation of MongoDB's BSON serialization (EOL)
 
 use version;
 our $VERSION = 'v0.8.4';
@@ -47,10 +47,20 @@ __END__
 
 =head1 END OF LIFE NOTICE
 
-Version v0.8.0 is the final feature release of the MongoDB BSON::XS
-library.  The library is now in a 12-month "sunset" period and will
-receive security patches and critical bug fixes only.  The BSON::XS
-library will be end-of-life and unsupported on August 13, 2020.
+Version v0.8.0 was the final feature release of the MongoDB BSON::XS
+library and v0.8.4 is the final patch release.
+
+B<As of August 13, 2020, the MongoDB Perl driver and related libraries have
+reached end of life and are no longer supported by MongoDB.> See the
+L<August 2019 deprecation
+notice|https://www.mongodb.com/blog/post/the-mongodb-perl-driver-is-being-deprecated>
+for rationale.
+
+If members of the community wish to continue development, they are welcome
+to fork the code under the terms of the Apache 2 license and release it
+under a new namespace.  Specifications and test files for MongoDB drivers
+and libraries are published in an open repository:
+L<mongodb/specifications|https://github.com/mongodb/specifications/tree/master/source>.
 
 =end :prelude
 
